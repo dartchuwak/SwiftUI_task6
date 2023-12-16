0f084322f9e63e7b769761bfd250cf9d8e8d6974
Evgenii Mikhailov <e.mikhailov@outlook.com>
2023-12-16 14:58:42 +0300

    Initial Commit

---
+++ SwiftUI_task6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_task6
+//
+//  Created by Evgenii Mikhailov on 16.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_task6/SwiftUI_task6App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_task6App.swift
+//  SwiftUI_task6
+//
+//  Created by Evgenii Mikhailov on 16.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_task6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
